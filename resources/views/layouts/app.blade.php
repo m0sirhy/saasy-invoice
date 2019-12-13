7cf643ec401728c32188909c4962dc8bf8e1f842
Guy Warner <gwarner@monitorbase.com>
2019-12-13 23:09:02 +0000

    Fix invoice Vue issue

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -81,7 +81,7 @@
 
     <div class="flex flex-col md:flex-row">
         @include('includes.nav')
-        <div id="app" class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
+        <div class="main-content flex-1 bg-gray-100 mt-12 md:mt-2 pb-24 md:pb-5">
             @yield('content')
         </div>
     </div>
