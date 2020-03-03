8f53271e196a4ca35245591be027c9157211ba63
Guy Warner <guy@mojomarketplace.com>
2020-03-03 13:26:28 -0700

    Add overdue invoice console (#70)

---
+++ app/Console/Commands/OverDue.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\Invoice;
+
+class OverDue extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'invoice:overdue';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Marks invoices as overdue';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        $invoices = Invoice::where('balance', '>', 0)
+            ->where('invoice_status_id', '!=', 5)
+            ->where('due_date', '<=', now())
+            ->get();
+        foreach ($invoices as $invoice) {
+            $invoice->invoice_status_id = 5;
+            $invoice->save();
+        }
+    }
+}
